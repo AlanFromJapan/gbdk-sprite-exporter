e399b6b3127a971a28438d4a023baf0c7e8dcb7e
alanfromjapan <alanfromjapan@gmail.com>
2022-12-01 22:50:13 +0900

    It works! Exports 1 16x16 sprite, but the coord are based on the Cel not absolute so to be fixed. Imported fine in GB rom.

--- gbdk-sprite-exporter.lua
+++ gbdk-sprite-exporter.lua
@@ -1,3 +1,34 @@
+local function exportTile2String (row, col, img)
+    local output = ""
+
+    --x,y are the PIXEL we look at in the Sprite coordinate (0,0) = top-left most point
+    for y = row * 8, (row + 1) * 8 -1, 1
+    do
+        local b1 = 0
+        local b2 = 0
+
+        for x = col * 8, (col +1) * 8 -1, 1
+        do
+            --indexed palette: get the color index of that pixel
+            idx = img:getPixel(x, y)
+
+            b1 = b1 << 1
+            b2 = b2 << 1
+
+            --from my C# program "GbReaper - Tile.cs" : the top and low part of the 2bit index of the color is split on 2 bytes
+            b1 = b1 | (0x01 & idx)
+            b2 = b2 | ((0x02 & idx) >> 1)
+
+        end --x    
+
+        if output ~= "" then
+            output = output .. ","
+        end
+        output = output .. string.format("0x%02x,0x%02x", b1, b2)
+    end --y
+
+    return output
+end
 
 local cel = app.activeCel
 if not cel then
@@ -10,12 +41,13 @@ if img.colorMode ~= ColorMode.INDEXED then
     return app.alert("This works only with indexed palettes.")
 end
 
---Correct image size is stored in app.activeSprite, cel is a subset of the sprite
+--Correct image size is stored in app.activeSprite, cel is a subset of the sprite (visible by pressing Ctrl)
 --https://community.aseprite.org/t/lua-scripts-image-size-does-not-match-actual-image-size/9766
 --print ("h " .. app.activeSprite.height .. " / w " .. app.activeSprite.width)
 if not (app.activeSprite.height % 16 == 0 and app.activeSprite.width % 16 == 0) then
     return app.alert("This works only with images that sizes are multiples of 16x16.")
 end
+
 
 local output = ""
 local n = #app.activeSprite.palettes[1]
@@ -27,33 +59,19 @@ if n > 2 then
   local row = 0
   local col = 0
 
-  --x,y are the PIXEL we look at in the Sprite coordinate (0,0) = top-left most point
-  for y = row * 8, (row + 1) * 8 -1, 1
-  do
-    local b1 = 0
-    local b2 = 0
+  --16x16 sprites are stored left half and right half so the parsing order is TL - BL - TR - BR
+  output = output .. exportTile2String(row, col, img)
+  output = output .. ", \n" .. exportTile2String(row+1, col, img)
+  output = output .. ", \n" .. exportTile2String(row, col+1, img)
+  output = output .. ", \n" .. exportTile2String(row+1, col+1, img)
 
-    for x = col * 8, (col +1) * 8 -1, 1
-    do
-      --indexed palette: get the color index of that pixel
-      idx = img:getPixel(x, y)
-
-      b1 = b1 << 1
-      b2 = b2 << 1
-
-      --from my C# program "GbReaper - Tile.cs" : the top and low part of the 2bit index of the color is split on 2 bytes
-      -- b1 = (byte)(b1 | (byte)(0x01 & vIdx));
-      -- b2 = (byte)(b2 | (byte)((0x02 & vIdx) >> 1));
-      b1 = b1 | (0x01 & idx)
-      b2 = b2 | ((0x02 & idx) >> 1)
-
-    end --x    
-
-    if output ~= "" then
-      output = output .. ","
-    end
-    output = output .. string.format("0x%02x,0x%02x", b1, b2)
-  end --y
+    --TODO add iteration through the whole image here
 end
 
-print(output)
+
+--and save because I can't copy the content of the print popup..
+local file = io.open(app.activeSprite.filename .. "-export.c","w")
+file:write(output)
+file:close()
+
+return app.alert("Done!")
